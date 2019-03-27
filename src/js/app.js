a40e4dae67b1b740281cb85ef633527a499888bc
Скрижалина Екатерина Александровна <skrizhalina@polymedia.ru>
2019-03-27 17:25:20 +0300

    fix: Correct syntax and variable names

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,16 @@
 const calculate_bonus = (cart) => {
   "use strict";
-  if (cart.isArray()) && (!cart.some(isNaN)) {
-    let cart_sum = 0;
-    for (let item of cart) {
-      sum += item;
+  if (Array.isArray(cart)) {
+    if (!cart.some(isNaN)) {
+      let cart_sum = 0;
+      for (let item of cart) {
+        cart_sum += item;
+      }
+      return cart_sum > 10000 ? Math.round(cart_sum * 0.05) : 0;
+    } else {
+      console.log("02 Input contains wrong values");
+      return 0;
     }
-    sum > 10000 ? Math.round(sum * 0.05) : 0;
   } else {
     console.log("01 Input is not a user cart");
     return 0;
