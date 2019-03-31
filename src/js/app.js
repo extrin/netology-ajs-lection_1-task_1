a47b22b25a3aebb8fe1cb734ca5d6ad6708e43e1
E.S <extrin13@gmail.com>
2019-03-31 19:38:37 +0300

    feat: Change maths for bonus calculation

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,11 @@
-const calculate_bonus = (cart) => {
-  "use strict";
+const calculate_bonus = cart => {
   if (Array.isArray(cart)) {
     if (!cart.some(isNaN)) {
       let cart_sum = 0;
       for (let item of cart) {
         cart_sum += item;
       }
-      return cart_sum > 10000 ? Math.round(cart_sum * 0.05) : 0;
+      return cart_sum > 10000 ? Math.round((cart_sum - 10000) * 0.05) : 0;
     } else {
       console.log("02 Input contains wrong values");
       return 0;
